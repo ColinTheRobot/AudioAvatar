7802d3564ebd0a82af811afe4f55d70249c1437e
catm705 <catgelinas78@gmail.com>
2014-05-31 19:13:50 -0400

    Got boxes lighting up with jQuery

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,24 +1,39 @@
 $(document).ready(function(){
-  for(n=0; n<=7; n++){
+  for(n=1; n<100; n++){
     var col_id = "column" + n;
-    var colDiv = $('<div/>').attr('id', col_id).appendTo("#chart");
-
-    for (i = 0; i <= 100; i++){
-      var arr_id = "array" + i;
-      var arrayDiv = $('<div>').attr('id', arr_id);
-      var miniDivs = $('<div>');
-      var divBoxes = miniDivs.addClass("box").appendTo(arrayDiv);
-      $(divBoxes).appendTo(rowDiv);
-    }
+    // alert(col_id);
+    var colDiv = $('<div/>').attr('id', col_id).addClass("vertical").appendTo("#chart");
+      var redDiv = $('<div>').addClass("box red").appendTo(colDiv);
+      var orangeDiv = $('<div>').addClass("box orange").appendTo(colDiv);
+      var yellowDiv = $('<div>').addClass("box yellow").appendTo(colDiv);
+      var greenDiv = $('<div>').addClass("box green").appendTo(colDiv);
+      var blueDiv = $('<div>').addClass("box blue").appendTo(colDiv);
+      var indigoDiv = $('<div>').addClass("box indigo").appendTo(colDiv);
+      var purpleDiv = $('<div>').addClass("box purple").appendTo(colDiv);
   }
 
-  var rand = Math.floor(Math.random() * 100) + 1;
-  var rand_colId = "column" + rand;
+  function lightUp(){
+
+    var colorArr = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
+    var randArrIndex = Math.floor(Math.random() * 6) + 1;
+    var color = colorArr[randArrIndex];
+
+    var lightedBox = "." + colorArr[randArrIndex];
+//     console.log(lightedBox);
+
+    var rand = Math.floor(Math.random() * 100) + 1;
+    var rand_col_Id = "#column" + rand;
+
+    var litUpThingIds = rand_col_Id + " " + lightedBox;
+    console.log(litUpThingIds);
 
 
+    $(litUpThingIds).css({'background-color': color});
 
+  }
 
-
-
+  setInterval(function() {
+      lightUp();
+  }, 500);
 
 });
