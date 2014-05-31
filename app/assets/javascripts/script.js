a9a457ace8bc49b0e08c3da2e2c895469abb7957
catm705 <catgelinas78@gmail.com>
2014-05-31 19:33:07 -0400

    Added a light up coloring method

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,35 +3,34 @@ $(document).ready(function(){
     var col_id = "column" + n;
     // alert(col_id);
     var colDiv = $('<div/>').attr('id', col_id).addClass("vertical").appendTo("#chart");
-      var redDiv = $('<div>').addClass("box red").appendTo(colDiv);
-      var orangeDiv = $('<div>').addClass("box orange").appendTo(colDiv);
-      var yellowDiv = $('<div>').addClass("box yellow").appendTo(colDiv);
-      var greenDiv = $('<div>').addClass("box green").appendTo(colDiv);
-      var blueDiv = $('<div>').addClass("box blue").appendTo(colDiv);
-      var indigoDiv = $('<div>').addClass("box indigo").appendTo(colDiv);
       var purpleDiv = $('<div>').addClass("box purple").appendTo(colDiv);
+      var indigoDiv = $('<div>').addClass("box indigo").appendTo(colDiv);
+      var blueDiv = $('<div>').addClass("box blue").appendTo(colDiv);
+      var greenDiv = $('<div>').addClass("box green").appendTo(colDiv);
+      var yellowDiv = $('<div>').addClass("box yellow").appendTo(colDiv);
+      var orangeDiv = $('<div>').addClass("box orange").appendTo(colDiv);
+      var redDiv = $('<div>').addClass("box red").appendTo(colDiv);
   }
 
   function lightUp(){
-
+    // color array to randomly choose colors to brighten
     var colorArr = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
     var randArrIndex = Math.floor(Math.random() * 6) + 1;
     var color = colorArr[randArrIndex];
-
     var lightedBox = "." + colorArr[randArrIndex];
-//     console.log(lightedBox);
 
+    // generates random column number
     var rand = Math.floor(Math.random() * 100) + 1;
     var rand_col_Id = "#column" + rand;
 
+    // This concats the random column id and box class
     var litUpThingIds = rand_col_Id + " " + lightedBox;
-    console.log(litUpThingIds);
-
 
     $(litUpThingIds).css({'background-color': color});
 
   }
 
+  // Randomly lights up colored divs
   setInterval(function() {
       lightUp();
   }, 500);
